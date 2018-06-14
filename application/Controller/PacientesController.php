c40315e1fc0bf7cde056e97d073141d1a8d8fddf
Yhoan Andres Galeano Urrea <yhoangaleano@gmail.com>
2018-06-14 15:20:03 -0500

    Primera parte MINI3

---
+++ application/Controller/PacientesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Mini\Controller;
+
+class PacientesController
+{
+    public function index(){
+
+        require APP. 'view/_templates/header.php';
+
+        require APP. 'view/pacientes/index.php';
+
+        require APP. 'view/_templates/footer.php';
+    }
+
+    public function guardar(){
+        
+    }
+
+    public function actualizar(){
+        
+    }
+
+    public function eiminar(){
+        
+    }
+}
